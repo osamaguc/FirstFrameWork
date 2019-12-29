005422bca417cc15298693251d79547adc096036
osama rabie <osamaguc@gmail.com>
2019-12-29 08:11:07 +0200

    1- The init pod creation

---
+++ FirstFrameWork/FirstFrameWork/FirstFrameWork.h
@@ -0,0 +1,19 @@
+//
+//  FirstFrameWork.h
+//  FirstFrameWork
+//
+//  Created by Osama Rabie on 29/12/2019.
+//  Copyright © 2019 Osama Rabie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FirstFrameWork.
+FOUNDATION_EXPORT double FirstFrameWorkVersionNumber;
+
+//! Project version string for FirstFrameWork.
+FOUNDATION_EXPORT const unsigned char FirstFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirstFrameWork/PublicHeader.h>
+
+
---
+++ FirstFrameWork/FirstFrameWork/service.swift
@@ -0,0 +1,25 @@
+//
+//  service.swift
+//  FirstFrameWork
+//
+//  Created by Osama Rabie on 29/12/2019.
+//  Copyright © 2019 Osama Rabie. All rights reserved.
+//
+
+import Foundation
+
+
+public class Service {
+    
+    private init (){}
+    
+    public static func string() -> String
+    {
+        return UUID.init().uuidString;
+    }
+    
+    public static func integer() -> Int
+    {
+        return Int(arc4random())
+    }
+}
